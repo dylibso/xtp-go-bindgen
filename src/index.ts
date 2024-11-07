134aa745798b87d716ab599316eb0a3ba977432c
Benjamin Eckel <bhelx@users.noreply.github.com>
2024-11-07 08:53:37 -0600

    Refactor of bindgen code (#22)
    See https://github.com/dylibso/xtp-bindgen/pull/18

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,70 @@
 import ejs from "ejs";
-import { getContext, helpers, Property } from "@dylibso/xtp-bindgen";
+import { helpers, getContext, ObjectType, EnumType, ArrayType, XtpNormalizedType, MapType, Parameter, Property, XtpTyped } from "@dylibso/xtp-bindgen"
 
-function toGolangType(property: Property): string {
-  if (property.$ref) return goName(property.$ref.name);
-  switch (property.type) {
-    case "string":
-      if (property.format === "date-time") {
-        return "time.Time";
+function toGolangTypeX(type: XtpNormalizedType): string {
+  // turn into reference pointer if needed
+  const pointerify = (t: string) => {
+    return `${type.nullable ? '*' : ''}${t}`
+  }
+
+  switch (type.kind) {
+    case 'string':
+      return pointerify('string')
+    case 'int32':
+      return pointerify('int32')
+    case 'float':
+      return pointerify('float32')
+    case 'double':
+      return pointerify('float64')
+    case 'byte':
+      return pointerify('byte')
+    case 'date-time':
+      return pointerify('time.Time')
+    case 'boolean':
+      return pointerify('bool')
+    case 'array':
+      const arrayType = type as ArrayType
+      return pointerify(`[]${toGolangTypeX(arrayType.elementType)}`)
+    case 'buffer':
+      return pointerify('[]byte')
+    case 'object':
+      const oType = (type as ObjectType)
+      if (oType.properties?.length > 0) {
+        return pointerify(goName(oType.name))
+      } else {
+        // let's use empty interface for an untyped object
+        return pointerify("interface{}")
       }
-      return "string";
-    case "number":
-      if (property.format === "float") {
-        return "float32";
-      }
-      if (property.format === "double") {
-        return "float64";
-      }
-      return "int64";
-    case "integer":
-      return "int32";
-    case "boolean":
-      return "bool";
-    case "object":
-      return "map[string]interface{}";
-    case "array":
-      if (!property.items) return "[]any";
-      // TODO this is not quite right to force cast
-      return `[]${toGolangType(property.items as Property)}`;
-    case "buffer":
-      return "[]byte";
+    case 'enum':
+      return pointerify(goName((type as EnumType).name))
+    case 'map':
+      const { keyType, valueType } = type as MapType
+      return pointerify(`map[${toGolangTypeX(keyType)}]${toGolangTypeX(valueType)}`)
     default:
-      throw new Error("Can't convert property to Go type: " + property.type);
+      throw new Error("Can't convert XTP type to Go type: " + type)
   }
 }
 
-function pointerToGolangType(property: Property) {
-  const typ = toGolangType(property);
+function toGolangType(property: XtpTyped, required?: boolean): string {
+  const t = toGolangTypeX(property.xtpType)
 
-  if (typ.startsWith("[]") || typ.startsWith("map[")) {
-    return typ;
-  }
+  // if required is unset, just return what we get back
+  if (required === undefined) return t
 
-  return `*${typ}`;
+  // if it's set and true, just return what we get back
+  if (required) return t
+
+  // otherwise it's false, so let's ensure it's a pointer
+  if (t.startsWith('*')) return t
+  return `*${t}`
+}
+
+// used to define a return type, for backwards compat
+function toGolangReturnType(property: Property): string {
+  const t = toGolangTypeX(property.xtpType)
+
+  if (t.startsWith('[]') || t.startsWith('map[')) return t
+  return `*${t}`
 }
 
 function makePublic(s: string) {
@@ -55,6 +78,7 @@ function makePublic(s: string) {
 }
 
 function goName(s: string) {
+  if (!s) throw Error('Name missing to convert')
   return makePublic(helpers.snakeToCamelCase(s));
 }
 
@@ -64,7 +88,7 @@ export function render() {
     ...helpers,
     ...getContext(),
     toGolangType,
-    pointerToGolangType,
+    toGolangReturnType,
     makePublic,
     goName,
   };
